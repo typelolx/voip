649af66bd80e6a1f9d6ec920af7b2f81e4cea276
Alexander <typelolpro@gmail.com>
2013-08-11 21:05:45 +0400

    created sipmanager

---
+++ src/com/zt/sip/IncomingCallReceiver.java
@@ -0,0 +1,13 @@
+package com.zt.sip;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class IncomingCallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
--- src/com/zt/voip/MainScreen.java
+++ src/com/zt/voip/MainScreen.java
@@ -1,16 +1,31 @@
 package com.zt.voip;
 
 import android.app.Activity;
+import android.net.sip.SipManager;
 import android.os.Bundle;
 
 public class MainScreen extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    public SipManager mSipManager;
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+
+    private void init(){
+
+        //инициализируем сип манагер
+        if(mSipManager == null) {
+            mSipManager = SipManager.newInstance(this);
+        }
+
+
+
     }
 
 }
